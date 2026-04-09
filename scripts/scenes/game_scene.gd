a10c1d7632f179def12906d67833a9630b8c0974
SpykieLucilius <spykie.lucilius@pm.me>
2026-04-09 23:56:47 +0300

    creating Game scene and adding Cheese Spawn script

--- scripts/scenes/game_scene.gd
+++ scripts/scenes/game_scene.gd
@@ -6,3 +6,49 @@ extends Node2D
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/menus/start_screen.tscn")
+
+# ------------------------------------------------------------------
+# Cheese tiles placement functions
+# ------------------------------------------------------------------
+
+const CheeseScene = preload("res://scenes/game/cheese.tscn")
+const COLUMNS = 6
+const ROWS = 3
+const CHEESE_WIDTH = 80
+const CHEESE_HEIGHT = 20
+const GAP_X = 10
+const GAP_Y = 10
+
+var PADDLE1_Y = 150.0
+var PADDLE2_Y = 498.0
+var MARGIN = 20.0
+var cheese_grid__height = ROWS * CHEESE_HEIGHT + (ROWS - 1) * GAP_Y
+
+func _ready():
+	spawn_cheese()
+
+func spawn_cheese():
+	var total_width = COLUMNS * CHEESE_WIDTH + (COLUMNS - 1) * GAP_X
+	var start_x = (1152.0 / 2.0) - (total_width / 2.0) + (CHEESE_WIDTH / 2.0)
+
+	# player 1 cheese placement
+	var p1_start_y = PADDLE1_Y - MARGIN - cheese_grid__height + CHEESE_HEIGHT / 2.0
+	for row in range(ROWS):
+		for col in range(COLUMNS):
+			var cheese = CheeseScene.instantiate()
+			cheese.position = Vector2(
+				start_x + col * (CHEESE_WIDTH + GAP_X),
+				p1_start_y + row * (CHEESE_HEIGHT + GAP_Y)
+			)
+			add_child(cheese)
+	
+	# player 2 cheese placement
+	var p2_start_y = PADDLE2_Y + MARGIN + CHEESE_HEIGHT / 2.0
+	for row in range(ROWS):
+		for col in range(COLUMNS):
+			var cheese = CheeseScene.instantiate()
+			cheese.position = Vector2(
+				start_x + col * (CHEESE_WIDTH + GAP_X),
+				p2_start_y + row * (CHEESE_HEIGHT + GAP_Y)
+			)
+			add_child(cheese)
