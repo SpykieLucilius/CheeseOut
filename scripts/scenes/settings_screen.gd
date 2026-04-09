586165f405612441be993d334d72da0a85aa4270
SpykieLucilius <spykie.lucilius@pm.me>
2026-04-09 17:28:51 +0300

    adding game scene and navigation

--- scripts/scenes/settings_screen.gd
+++ scripts/scenes/settings_screen.gd
@@ -1,4 +1,8 @@
 extends Node2D
 
+# ------------------------------------------------------------------
+# Menu navigation functions
+# ------------------------------------------------------------------
+
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/menus/start_screen.tscn")
